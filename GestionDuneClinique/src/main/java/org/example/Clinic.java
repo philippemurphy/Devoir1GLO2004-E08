10eb9b66bbc7b8c65a6b4a53b0ffae2d8480a714
David9322 <41498319+David9322@users.noreply.github.com>
2024-09-09 19:00:13 -0400

    Add files via upload

---
+++ GestionDuneClinique/src/main/java/org/example/Clinic.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Clinic {
+    public Clinic(/* ... */) {
+    }
+
+    public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {
+        // TODO
+    }
+
+    // D'autres méthodes peuvent être nécessaires
+}
